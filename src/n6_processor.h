7b423bdfc68fd6c36cd27dafd96be96f6a79f8ad
張耀文 <hadeschang0322.cs04@g2.nctu.edu.tw>
2020-11-14 22:00:58 +0000

    Use marco to write pkt handler and throughput upto 39Gb

--- src/n6_processor.h
+++ src/n6_processor.h
@@ -23,8 +23,7 @@ extern pkt_stats_t port_pkt_stats[GTP_CFG_MAX_PORTS];
  *   - < 0 if packet processes failed, need to free this packet from caller
  */
 static __rte_always_inline int process_n6(struct rte_mbuf *m, interface_t *interface, struct rte_ipv4_hdr *ipv4_hdr)
-{  
-    int ret;
+{
     int out_int = interface->id ^ 1;
 
     rule_match_t *rule_match = NULL;
@@ -39,17 +38,24 @@ static __rte_always_inline int process_n6(struct rte_mbuf *m, interface_t *inter
     switch(rule_action->apply_action) {
         case RULE_ACTION_APPLY_ACTION_FORW:
             printf_dbg(" , need to forward");
-            if (unlikely((ret = process_outer_hdr_removal(m, rule_match, ipv4_hdr)) < 0)) {
-                printf_dbg(", ERROR: cannot handle outer hdr removal");
-                return ret;
-            }
 
-            if (unlikely((ret = process_outer_hdr_creation(m, rule_action, out_int)) < 0)) {
-                printf_dbg(", ERROR: cannot handle outer hdr creation");
-                return ret;
-            }
+            process_outer_hdr_removal_macro(rule_match->remove_hdr,
+                    process_outer_hdr_removal_none_macro(m, break),
+                    /* N6 ingress port don't support GTP-U decap */,
+                    return -EPROTONOSUPPORT
+            );
 
-            return process_egress_inplace(m, interface, out_int);
+            struct rte_ether_hdr *eth_hdr;
+            struct rte_udp_hdr *udp_hdr;
+            struct rte_gtp_hdr *gtp_hdr;
+            uint16_t payload_len;
+            process_outer_hdr_creation_macro(rule_action->outer_hdr_info.desp,
+                process_outer_hdr_creation_gtpu_ipv4_macro(m, eth_hdr, ipv4_hdr, udp_hdr, gtp_hdr, payload_len, rule_action, out_int, break),
+                process_outer_hdr_creation_nono_macro(m, eth_hdr, break),
+                return -EPROTONOSUPPORT
+            );
+
+            process_egress_marco(m, interface, eth_hdr, ipv4_hdr, out_int, return 0);
         case RULE_ACTION_APPLY_ACTION_DROP:
             printf_dbg(" , need to drop");
             rte_pktmbuf_free(m);
@@ -75,23 +81,21 @@ static __rte_always_inline void n6_processor(struct rte_mbuf *m, interface_t *in
     if (unlikely(rte_pktmbuf_data_len(m) < RTE_ETHER_HDR_LEN + sizeof(struct rte_ipv4_hdr)))
         goto FREE_PKT_MBUF;
 
-    int ret;
+    int offset;
 
     // Process frame
-    struct rte_ether_hdr *eth_hdr =NULL;
-    int offset = parse_frame_mbuf(m, interface, &eth_hdr);
+    struct rte_ether_hdr *eth_hdr = NULL;
+    parse_frame_mbuf_marco(m, interface, eth_hdr);
+    offset = RTE_ETHER_HDR_LEN;
 
     struct rte_ipv4_hdr *ipv4_hdr = NULL;
     switch (rte_be_to_cpu_16(eth_hdr->ether_type)) {
         // Order of ether_type in switch case using happening frequency
         case RTE_ETHER_TYPE_IPV4:
             printf_dbg(";");
-            if (unlikely((ret = parse_pkt_mbuf(m, interface, offset, &ipv4_hdr)) < 0)) {
-                printf_dbg(" Warning: cannot handle IPv4 packet \n");
-                goto FREE_PKT_MBUF;
-            }
 
-            offset += ret;
+            parse_pkt_mbuf_macro(m, interface, eth_hdr, ipv4_hdr, goto FREE_PKT_MBUF);
+            offset += sizeof(struct rte_ipv4_hdr);
             break;
         case RTE_ETHER_TYPE_ARP:
             if (unlikely(arp_in(m, interface))) {
--- src/pkt.h
+++ src/pkt.h
@@ -19,178 +19,154 @@
 extern pkt_stats_t port_pkt_stats[GTP_CFG_MAX_PORTS];
 
 /**
- * Used to store network header pointer when parse mbuf
- * 
- * TODO: Remove it if the throughput is bad
-typedef struct mbuf_network_info_s {
-    interface_t             *interface;
+ * @m: struct rte_mbuf *
+ * @interface: interface_t *
+ * @eth_hdr: struct rte_ether_hdr *
+ */
+#define parse_frame_mbuf_marco(m, interface, eth_hdr) \
+    eth_hdr = rte_pktmbuf_mtod(m, struct rte_ether_hdr *); \
+    printf_dbg("\n [RX] Port #%u (%s) ", interface->id, (interface->type == INTERFACE_TYPE_N6 ? "N6" : "GTP-U")); \
+    print_dbg_ether_hdr_mac(eth_hdr);
 
-    struct rte_ether_hdr    *eth_hdr;
-    struct rte_ipv4_hdr     *ipv4_hdr;
-    struct rte_udp_hdr      *udp_hdr;
-    struct rte_gtp_hdr      *gtp_hdr;
+/**
+ * @m: struct rte_mbuf *
+ * @interface: interface_t *
+ * @eth_hdr: struct rte_ether_hdr *
+ * @ipv4_hdr: struct rte_ipv4_hdr *
+ * @err_handle_expr: expr for free buffer
+ */
+#define parse_pkt_mbuf_macro(m, interface, eth_hdr, ipv4_hdr, err_handle_expr) \
+    ipv4_hdr = (void *) &eth_hdr[1]; \
+    print_dbg_ipv4_hdr_addr(ipv4_hdr); \
+    /* Check IP is fragmented */ \
+    if (unlikely(rte_ipv4_frag_pkt_is_fragmented(ipv4_hdr))) { \
+        port_pkt_stats[interface->id].ipFrag += 1; \
+        printf_dbg(", detected ip fragment and not support yet"); \
+        err_handle_expr; \
+    }
 
-    union origin_t {
-        struct rte_ether_hdr    *eth_hdr;
-        struct rte_ipv4_hdr     *ipv4_hdr;
-    } origin;
+/**
+ * @m: struct rte_mbuf *
+ * @interface: interface_t *
+ * @eth_hdr: struct rte_ether_hdr *
+ * @ipv4_hdr: struct rte_ipv4_hdr *
+ * @out_int: egress interface ID
+ * @suss_handle_expr: expr for successful handling
+ */
+#define process_egress_marco(m, interface, eth_hdr, ipv4_hdr, out_int, suss_handle_expr) \
+    printf_dbg(", find out dst IP "); \
+    print_dbg_ipv4(ipv4_hdr->dst_addr); \
+    printf_dbg(" in ARP table:"); \
+    if (unlikely(arp_get_mac(ipv4_hdr->dst_addr, &eth_hdr->d_addr) < 0)) { \
+        printf_dbg(" not found in arp table"); \
+        port_pkt_stats[interface->id].dropped += 1; \
+        /* Send ARP request and drop this packet */ \
+        arp_send_request(ipv4_hdr->dst_addr, out_int); \
+        return -ENOENT; \
+    } \
+    printf_dbg(" with dst mac "); \
+    print_dbg_mac(&eth_hdr->d_addr); \
+    rte_ether_addr_copy(&eth_hdr->s_addr, &interface_get_this(out_int)->mac); \
+    eth_hdr->ether_type = rte_cpu_to_be_16(RTE_ETHER_TYPE_IPV4); \
+    /* Put packet into TX queue */ \
+    int nb_tx; \
+    printf_dbg("\n [TX]"); \
+    if (unlikely((nb_tx = rte_eth_tx_burst(out_int, 0, &m, 1)) < 1)) { \
+        printf_dbg(" ERR(rte_eth_tx_burst=%d) \n", nb_tx); \
+        return -ENOSPC; \
+    } \
+    printf_dbg(" Sent frame to Port #%d with nb_tx=%d \n", out_int, nb_tx); \
+    suss_handle_expr;
     
-} mbuf_network_info_t;
-*/
 
 /**
- * @return
- *   - length of hdr
- *   - < 0 if do not handle this packet
+ * @m: struct rte_mbuf *
+ * @inner_ipv4_hdr: struct rte_ipv4_hdr *
+ * @ipv4_hdr: struct rte_ipv4_hdr *
+ * @suss_handle_expr: expr for successful handling
  */
-static __rte_always_inline int parse_frame_mbuf(struct rte_mbuf *m,
-        __attribute__((unused)) interface_t *interface, struct rte_ether_hdr **eth_hdr)
-{   
-    *eth_hdr = rte_pktmbuf_mtod(m, struct rte_ether_hdr *);
-    printf_dbg("\n [RX] Port #%u (%s) ", interface->id, (interface->type == INTERFACE_TYPE_N6 ? "N6" : "GTP-U"));
-    print_dbg_ether_hdr_mac(*eth_hdr);
-
-    return RTE_ETHER_HDR_LEN;
-}
+#define process_outer_hdr_removal_gtpu_ipv4_macro(m, inner_ipv4_hdr, ipv4_hdr, suss_handle_expr) \
+    case RULE_MATCH_REMOVE_HDR_COOKED_GTPU_IPV4: \
+        printf_dbg(", remove GTP-U, UDP, IPv4 and ethernet hdr"); \
+        ipv4_hdr = (struct rte_ipv4_hdr *) rte_pktmbuf_adj(m, (uint8_t *) inner_ipv4_hdr - rte_pktmbuf_mtod(m, uint8_t *)); \
+        suss_handle_expr;
 
 /**
- * @return
- *   - length of hdr
- *   - < 0 if do not handle this packet
+ * @m: struct rte_mbuf *
+ * @suss_handle_expr: expr for successful handling
  */
-static __rte_always_inline int parse_pkt_mbuf(struct rte_mbuf *m,
-        interface_t *interface, uint16_t offset, struct rte_ipv4_hdr **ipv4_hdr)
-{   
-    *ipv4_hdr = rte_pktmbuf_mtod_offset(m, struct rte_ipv4_hdr *, offset);
-    print_dbg_ipv4_hdr_addr(*ipv4_hdr);
-    
-    // Check IP is fragmented
-    if (unlikely(rte_ipv4_frag_pkt_is_fragmented(*ipv4_hdr))) {
-        port_pkt_stats[interface->id].ipFrag += 1;
-        *ipv4_hdr = NULL;
-        printf_dbg(", detected ip fragment and not support yet");
-        return -EPROTONOSUPPORT;
-    }
-
-    return (int) sizeof(struct rte_ipv4_hdr);
-}
+#define process_outer_hdr_removal_none_macro(m, suss_handle_expr) \
+    case RULE_MATCH_REMOVE_HDR_COOKED_UNSPEC: \
+        printf_dbg(", remove ethernet hdr"); \
+        rte_pktmbuf_adj(m, RTE_ETHER_HDR_LEN); \
+        suss_handle_expr;
 
 /**
- * @return
- *   - 0 if packet processes successfully
- *   - < 0 if packet processes failed, need to free this packet from caller
+ * @remove_hdr: uint8_t
+ * @rm_hdr_expr_case1: first priority case expr
+ * @rm_hdr_expr_case2: second priority case expr
+ * @err_handle_expr: expr for free buffer
  */
-static __rte_always_inline int process_egress_inplace(struct rte_mbuf *m, interface_t *interface, uint8_t out_int)
-{
-    struct rte_ether_hdr *eth_hdr = rte_pktmbuf_mtod(m, struct rte_ether_hdr *);
-    struct rte_ipv4_hdr *ipv4_hdr = (void *) &eth_hdr[1];
-    
-    printf_dbg(", find out dst IP ");
-    print_dbg_ipv4(ipv4_hdr->dst_addr);
-    printf_dbg(" in ARP table:");
-    if (unlikely(arp_get_mac(ipv4_hdr->dst_addr, &eth_hdr->d_addr) < 0)) {
-        printf_dbg(" not found in arp table");
-        port_pkt_stats[interface->id].dropped += 1;
-
-        arp_send_request(ipv4_hdr->dst_addr, out_int);
-        return -ENOENT;
+#define process_outer_hdr_removal_macro(remove_hdr, rm_hdr_expr_case1, rm_hdr_expr_case2, err_handle_expr) \
+    switch(remove_hdr) { \
+        rm_hdr_expr_case1; \
+        rm_hdr_expr_case2; \
+        default: \
+            printf_dbg(", not support IPv6 hdr yet"); \
+            err_handle_expr; \
     }
-    else {
-        printf_dbg(" with dst mac ");
-        print_dbg_mac(&eth_hdr->d_addr);
-    }
-
-    rte_ether_addr_copy(&eth_hdr->s_addr, &interface_get_this(out_int)->mac);
-    // TODO: Set ether_type with payload
-    eth_hdr->ether_type = rte_cpu_to_be_16(RTE_ETHER_TYPE_IPV4);
-
-    int nb_tx;
-    printf_dbg("\n [TX]");
-    if (likely((nb_tx = rte_eth_tx_burst(out_int, 0, &m, 1)) == 1)) {
-        printf_dbg(" Sent frame to Port #%d with nb_tx=%d \n", out_int, nb_tx);
-        return 0;
-    }
-
-    printf_dbg(" ERR(rte_eth_tx_burst=%d) \n", nb_tx);
-    return -ENOSPC;
-}
 
 /**
- * @return
- *   - 0 if packet processes successfully
- *   - < 0 if packet processes failed, need to free this packet from caller
+ * @m: struct rte_mbuf *
+ * @eth_hdr: struct rte_ether_hdr *
+ * @ipv4_hdr: struct rte_ipv4_hdr *
+ * @udp_hdr: struct rte_udp_hdr *
+ * @gtp_hdr: struct rte_udp_hdr *
+ * @rule_action: rule_action_t *
+ * @out_int: interface id
+ * @suss_handle_expr: expr for successful handling
  */
-static __rte_always_inline int process_outer_hdr_removal(struct rte_mbuf *m,
-        rule_match_t *rule_match, struct rte_ipv4_hdr *ipv4_hdr)
-{
-    switch(rule_match->remove_hdr) {
-        case RULE_MATCH_REMOVE_HDR_COOKED_GTPU_IPV4: // More happen in ingress N3
-            printf_dbg(", remove GTP-U, UDP, IPv4 and ethernet hdr");
-            rte_pktmbuf_adj(m, (uint8_t *) ipv4_hdr - rte_pktmbuf_mtod(m, uint8_t *));
-            return 0;
-        case RULE_MATCH_REMOVE_HDR_COOKED_UNSPEC: // More happen in ingress N6
-            printf_dbg(", remove ethernet hdr");
-            rte_pktmbuf_adj(m, RTE_ETHER_HDR_LEN);
-            return 0;
-        /* TODO: not support yet
-        case RULE_MATCH_REMOVE_HDR_COOKED_UDP_IPV4:
-            printf_dbg(", remove UDP, IPv4 and ethernet hdr");
-            // rte_pktmbuf_adj(m, (uint8_t *) network_info->gtp_hdr - (uint8_t *) network_info->eth_hdr);
-            return -EPROTONOSUPPORT;
-        */
-        default:
-            printf_dbg(", not support IPv6 hdr yet");
-            return -EPROTONOSUPPORT;
-    }
-}
+#define process_outer_hdr_creation_gtpu_ipv4_macro(m, eth_hdr, ipv4_hdr, udp_hdr, gtp_hdr, payload_len, rule_action, out_int, suss_handle_expr) \
+    case RULE_ACTION_OUTER_HDR_DESP_GTPU_IPV4: \
+        printf_dbg(", create outer gtp-u, udp and ipv4 hdr"); \
+        eth_hdr = (struct rte_ether_hdr *) rte_pktmbuf_prepend(m, \
+                RTE_ETHER_GTP_HLEN + sizeof(struct rte_ipv4_hdr) + RTE_ETHER_HDR_LEN); \
+        /* ethernet hdr will be handled at send function */ \
+        ipv4_header_set_inplace((ipv4_hdr = (void *) &eth_hdr[1]), \
+                interface_get_this(out_int)->ipv4, rule_action->outer_hdr_info.peer_ipv4, \
+                (payload_len = rte_pktmbuf_data_len(m) - RTE_ETHER_HDR_LEN)); \
+        udp_header_set_inplace((udp_hdr = (void *) &ipv4_hdr[1]), \
+                0x6808, rule_action->outer_hdr_info.peer_port, \
+                (payload_len -= sizeof(struct rte_ipv4_hdr))); \
+        gtpu_header_set_inplace((gtp_hdr = (void *) &udp_hdr[1]), \
+                0, 0xff, (payload_len -= sizeof(struct rte_udp_hdr)), rule_action->outer_hdr_info.teid); \
+        suss_handle_expr;
 
 /**
- * @return
- *   - 0 if packet processes successfully
- *   - < 0 if packet processes failed, need to free this packet from caller
+ * @m: struct rte_mbuf *
+ * @eth_hdr: struct rte_ether_hdr *
+ * @suss_handle_expr: expr for successful handling
  */
-static __rte_always_inline int process_outer_hdr_creation(struct rte_mbuf *m,
-        rule_action_t *rule_action, uint8_t out_int)
-{
-    uint16_t payload_len;
-    struct rte_ether_hdr *eth_hdr;
-    struct rte_ipv4_hdr *ipv4_hdr;
-    struct rte_udp_hdr *udp_hdr;
-    struct rte_gtp_hdr *gtp_hdr;
+#define process_outer_hdr_creation_nono_macro(m, eth_hdr, suss_handle_expr) \
+    case RULE_ACTION_OUTER_HDR_DESP_UNSPEC: \
+        printf_dbg(", don't create outer hdr"); \
+        eth_hdr = (struct rte_ether_hdr *) rte_pktmbuf_prepend(m, sizeof(struct rte_ether_hdr)); \
+        /* ethernet hdr will be handled at send function */ \
+        suss_handle_expr;
 
-    switch(rule_action->outer_hdr_info.desp) {
-        case RULE_ACTION_OUTER_HDR_DESP_UNSPEC: // More happen in egress N6
-            printf_dbg(", don't create outer hdr");
-            rte_pktmbuf_prepend(m, sizeof(struct rte_ether_hdr));
-            // ethernet hdr will be handled at send function
-            return 0;
-        case RULE_ACTION_OUTER_HDR_DESP_GTPU_IPV4: // More happen in egress N3
-            printf_dbg(", create outer gtp-u, udp and ipv4 hdr");
-            eth_hdr = (struct rte_ether_hdr *) rte_pktmbuf_prepend(m,
-                    RTE_ETHER_GTP_HLEN + sizeof(struct rte_ipv4_hdr) + sizeof(struct rte_ether_hdr));
-            // ethernet hdr will be handled at send function
-
-            ipv4_header_set_inplace((ipv4_hdr = (void *) &eth_hdr[1]), 
-                    interface_get_this(out_int)->ipv4, rule_action->outer_hdr_info.peer_ipv4,
-                    (payload_len = rte_pktmbuf_data_len(m) - sizeof(struct rte_ether_hdr)));
-
-            udp_header_set_inplace((udp_hdr = (void *) &ipv4_hdr[1]),
-                    0x6808, rule_action->outer_hdr_info.peer_port,
-                    (payload_len -= sizeof(struct rte_ipv4_hdr)));
-
-            gtpu_header_set_inplace((gtp_hdr = (void *) &udp_hdr[1]),
-                    0, 0xff, (payload_len -= sizeof(struct rte_udp_hdr)), rule_action->outer_hdr_info.teid);
-            return 0;
-        /*
-        case RULE_ACTION_OUTER_HDR_DESP_UDP_IPV4:
-            // TODO: 
-            printf_dbg(" not support adding UDP and IPv4 hdr yet");
-            return -EPROTONOSUPPORT;
-        */
-        default:
-            printf_dbg(" not support IPv6 hdr yet");
-            return -EPROTONOSUPPORT;
+/**
+ * @create_hdr_desp: uint8_t
+ * @create_hdr_expr_case1: first priority case expr
+ * @create_hdr_expr_case2: second priority case expr
+ * @err_handle_expr: expr for free buffer
+ */
+#define process_outer_hdr_creation_macro(create_hdr_desp, create_hdr_expr_case1, create_hdr_expr_case2, err_handle_expr) \
+    switch (create_hdr_desp) { \
+        create_hdr_expr_case1; \
+        create_hdr_expr_case2; \
+        default: \
+            printf_dbg(", not support IPv6 hdr yet"); \
+            err_handle_expr; \
     }
-}
 
 #endif /* __DPDK_GTP_GW_PKT_H__ */
